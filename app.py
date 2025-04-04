d474b32ffaf85fe979039b40095a9a5a4ee9b9ff
overclocked-2124 <sumukhaupadhyaya@gmail.com>
2025-04-04 21:52:17 +0530

    added sign-up and login page yet to connect to db

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask, jsonify, request, render_template,url_for
+from flask import Flask, jsonify, request, render_template,url_for,flash, redirect
+from forms import RegistrationForm, LoginForm
+import email_validator
 
-# Create Flask app instance
 app = Flask(__name__)
 
-# Example route
+app.config['SECRET_KEY'] = 'c1877bdc3305c942f87b10f86e246167'
+
+
 @app.route('/')
 def home():
-    
     return render_template('index.html',title="HELIXR",css_path="index")
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!','success')
+        return redirect(url_for('home'))
+    return render_template('register.html',title="HELIXR-Register",css_path="register",form = form)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title="HELIXR-Login",css_path="login",form = form)
 
 
 if __name__ == '__main__':
