c2a2c6c6b0b286028c66f6cbc6e67554cfdcd170
overclocked-2124 <sumukhaupadhyaya@gmail.com>
2025-04-10 08:55:12 +0530

    added a user class database using SQLALchemy

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, jsonify, request, render_template,url_for,flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 import email_validator
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'c1877bdc3305c942f87b10f86e246167'
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.model):
+    id = db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(20), unique=True,nullable=False)
+    email = db.Column(db.String(120), unique=True,nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}' ,'{self.username}', '{self.image_file}')"
+
 
 
 @app.route('/')
@@ -31,7 +46,7 @@ def login():
                 flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title="HELIXR-Login", css_path="login", form=form)
 
-
+p
 
 if __name__ == '__main__':
     app.run(debug=True)
