1c3e9fb37629c746a894547d1ab51ebafe7252a1
overclocked-2124 <sumukhaupadhyaya@gmail.com>
2025-04-14 10:17:53 +0530

    changed Package structure

--- HeliXR/app.py
+++ HeliXR/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request, render_template,url_for,flash, redire
 from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 import email_validator
+from models import User
 
 app = Flask(__name__)
 
@@ -10,20 +11,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
 
 db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
-    username = db.Column(db.String(20), unique=True,nullable=False)
-    email = db.Column(db.String(120), unique=True,nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
-    password = db.Column(db.String(60), nullable=False)
-
-    def __repr__(self):
-        return f"User('{self.username}' ,'{self.email}', '{self.image_file}')"
-
-
-
-
 
 @app.route('/')
 def home():
