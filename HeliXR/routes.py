ef4633e39d4b00ac90db076f1259a42118027bdc
Sumukha_Upadhyaya <146051934+overclocked-2124@users.noreply.github.com>
2025-04-16 09:19:56 +0530

    Update routes.py

--- HeliXR/routes.py
+++ HeliXR/routes.py
@@ -27,7 +27,7 @@ def login():
     if request.method == 'POST':
         if form.validate_on_submit():
             user = User.query.filter_by(email=form.email.data).first()
-            if user and bcrypt. check_password_hash(user.password, form.password.data):
+            if user and bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user,remember=form.remember.data)
                 return redirect(url_for('home'))
             else:
